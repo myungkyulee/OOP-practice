3b9ac47dc1dadb0113a70c92f26675ba27ad39c1
Myungkyulee <102199101+Myungyulee@users.noreply.github.com>
2023-02-09 17:14:11 +0900

    feat : likelion api test

--- likelion/weapon/FireWand.java
+++
@@ -1,7 +0,0 @@
-package com.example.springtestlikelion.likelion.weapon;
-
-public class FireWand extends Weapon {
-    public FireWand() {
-        super("불지팡이", 50);
-    }
-}
--- likelion/weapon/IceWand.java
+++
@@ -1,7 +0,0 @@
-package com.example.springtestlikelion.likelion.weapon;
-
-public class IceWand extends Weapon {
-    public IceWand() {
-        super("얼음지팡이", 60);
-    }
-}
